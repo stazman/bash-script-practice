32adb817e8d0eb0640465daa7d4ddcd9dedd5085
Christopher DiStasio <cadistasio@gmail.com>
2020-12-28 09:24:28 -0600

    Adds less script file, test text files

---
+++ test-script-less.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#less command is to read text files of many kinds, such as README.md and txt. It will show the contents of text files in a separate terminal space than any previous commands before it
+
+# less README.md
+
+# less test-text-file.txt
+
+# opening more than one text file with less; then you can navigate back and forth between them with :n and :P
+
+less *
+
+# you can't run :n and :p as commands in a shell script because they only work within the editor
