984d90edda6a3dba9087390dcbe90712c4c5ad17
Christopher DiStasio <cadistasio@gmail.com>
2020-12-28 09:22:37 -0600

    Renames, adds more basic commands to first script

--- test-script-1.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# On a Mac, bash scripts go in root: /usr/local/bin
-
-# time related
-
-cal
-
-date
---
+++ test-script-basic-commands.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# These files are copied from .bash files placed in the Mac directory (root) /usr/local/bin because files need to be located in this exact directory for them to be executable
+
+# Bash files are made executable 
+
+# On a Mac, bash scripts go in root: /usr/local/bin
+
+# time related 
+
+cal
+
+date
+
+# locating things
+
+pwd
+
+exit #doesn't work in a file of commands but does on command line
+
+echo "Hello"
