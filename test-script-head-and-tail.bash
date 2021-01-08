5faa2fbcc64b2b6afd3602ccfa903e7322ca9004
Christopher DiStasio <cadistasio@gmail.com>
2021-01-07 21:16:50 -0600

    Adds, completes head and tail practice

---
+++ test-script-head-and-tail.bash
@@ -0,0 +1,12 @@
+# head prints out first 10 lines of file by default
+# tail prints out last 10 lines of file by default
+
+head test-text-file-1.txt
+tail test-text-file-1.txt
+
+
+# with specific number of lines
+
+head -n 5 test-text-file-1.txt
+tail -n 5 test-text-file-1.txt
+
